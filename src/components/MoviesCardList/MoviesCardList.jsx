d315a9267c8332eb170a797f4f20e75513eddbf8
Kristina <kristinafurman@proton.me>
2023-11-12 22:26:19 +0300

    feat: added trailer to movie card (#7)
    * feat: added trailer to movie card
    
    * chore: deleted lines

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -24,6 +24,7 @@ export function MoviesCardList({
           movie={movie}
           handleDelete={handleDelete}
           isDeleteAction={isDeleteAction}
+          trailerLink={movie.trailerLink}
         />
       ))}
     </ul>
